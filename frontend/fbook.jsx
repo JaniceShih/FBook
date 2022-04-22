dc465370117d8d9a3da70161dad98829a704f84b
JaniceShih <janiceshih@LAPTOP-2TCG6OI9.localdomain>
2022-04-22 17:58:50 -0400

    setup

---
+++ frontend/fbook.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// entry point
+document.addEventListener("DOMContentLoaded", ()=>{
+    const root = document.getElementById('root');
+
+    ReactDOM.render(<div>Hello Fbook!</div>, root);
+})
