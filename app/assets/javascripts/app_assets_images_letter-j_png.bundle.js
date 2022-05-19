aa411f36816b4c9b92b29a71ec9ab506579ba9e4
JaniceShih <janice.hc.shih@gmail.com>
2022-05-19 16:06:34 -0400

    change Fbook to Jacebook

--- app/assets/javascripts/app_assets_images_letter-j_png.bundle.js
+++ app/assets/javascripts/app_assets_images_letter-j_png.bundle.js
@@ -1,13 +1,16 @@
-(self["webpackChunkfbook"] = self["webpackChunkfbook"] || []).push([["app_assets_images_letter-j_png"],{
-
-/***/ "./app/assets/images/letter-j.png":
-/*!****************************************!*\
+(self["webpackChunkJaceBook"] = self["webpackChunkJaceBook"] || []).push([
+  ["app_assets_images_letter-j_png"],
+  {
+    /***/ "./app/assets/images/letter-j.png":
+      /*!****************************************!*\
   !*** ./app/assets/images/letter-j.png ***!
   \****************************************/
-/***/ (() => {
+      /***/ () => {
+        throw new Error(
+          "Module parse failed: Unexpected character '�' (1:0)\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\n(Source code omitted for this binary file)"
+        );
 
-throw new Error("Module parse failed: Unexpected character '�' (1:0)\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\n(Source code omitted for this binary file)");
-
-/***/ })
-
-}]);
+        /***/
+      },
+  },
+]);
