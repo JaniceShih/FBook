d49cbfb0aac5951cee63bbb12fc92ecf124b52be
JaniceShih <janice.hc.shih@gmail.com>
2022-05-19 14:34:56 -0400

    add seed file

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -2,10 +2,12 @@ class Friend < ApplicationRecord
 
     belongs_to :user,
         foreign_key: :user_id,
-        class_name: :User
+        class_name: :User,
+        optional: true
 
     belongs_to :friend,
         foreign_key: :friend_id,
-        class_name: :User
+        class_name: :User,
+        optional: true
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,29 +8,40 @@
 
 # demoUser = User.create!(fname: 'Demo', lname: 'User', email: 'demo@gmail.com', birthday: '2002-02-5', gender: 'Female',  bio: '', password: '123456')
 
+
+
+Like.destroy_all
+Comment.destroy_all
+Post.destroy_all
+Friend.destroy_all
+User.destroy_all
+
+demo = User.create!(fname: 'Demo', lname: 'User', email: 'demo@gmail.com', birthday: '1999-06-06', gender: 'Male',  bio: '', password: '123456')
 joe = User.create!(fname: 'Joe', lname: 'Delaney', email: 'joe@gmail.com', birthday: '1999-06-06', gender: 'Male',  bio: '', password: '123456')
-dom = qui
 jackson = User.create!(fname: 'Jackson', lname: 'Dooley', email: 'jackson@gmail.com', birthday: '2002-05-2', gender: 'Male',  bio: '', password: '123456')
 kevin = User.create!(fname: 'Kevin', lname: 'Tung', email: 'kevin@gmail.com', birthday: '1992-05-2', gender: 'Male',  bio: '', password: '123456')
 nick = User.create!(fname: 'Nicholas', lname: 'Angleton', email: 'nick@gmail.com', birthday: '1992-05-2', gender: 'Male',  bio: '', password: '123456')
-naran = User.create!(fname: 'Naran', lname: 'Ivanchukov', email: 'naran@gmail.com', birthday: '1992-05-2', gender: 'Male',  bio: '', password: '123456')
+dom = User.create!(fname: 'Dominic', lname: 'Swaby', email: 'dom@gmail.com', birthday: '1992-05-2', gender: 'Male',  bio: '', password: '123456')
 
 
-Friend.create!(user_id: 1, friend_id: 2, status: "Friends")
-Friend.create!(user_id: 1, friend_id: 3, status: "Friends")
-Friend.create!(user_id: 1, friend_id: 4, status: "Friends")
-Friend.create!(user_id: 1, friend_id: 5, status: "Friends")
-Friend.create!(user_id: 1, friend_id: 6, status: "Friends")
-Friend.create!(user_id: 1, friend_id: 7, status: "Friends")
-
-Friend.create!(user_id: 2, friend_id: 3, status: "Friends")
-Friend.create!(user_id: 2, friend_id: 3, status: "Friends")
-
-Comment.create!(body: "Slack clone", user_id: 2, post_id: 5)
-Comment.create!(body: "Splitwise clone", user_id: 3, post_id: 5)
+Friend.create!(user_id: demo.id, friend_id: joe.id , status: "Friends")
+Friend.create!(user_id: demo.id, friend_id: jackson.id, status: "Friends")
+Friend.create!(user_id: demo.id, friend_id: kevin.id, status: "Friends")
+Friend.create!(user_id: demo.id, friend_id: nick.id, status: "Friends")
+Friend.create!(user_id: demo.id, friend_id: dom.id, status: "Friends")
 
 
-Like.create!(like_id: 5, like_type: "Post", user_id: 1)
-Like.create!(like_id: 5, like_type: "Post", user_id: 2)
-Like.create!(like_id: 1, like_type: "Comment", user_id: 1)
-Like.create!(like_id: 2, like_type: "Comment", user_id: 2)
+post1 = Post.create!(body: 'Starting coding', user_id: demo.id)
+post2= Post.create!(body: 'Full Stack Project', user_id: demo.id)
+
+
+commit1 = Comment.create!(body: "Slack clone", user_id: joe.id, post_id: post2.id)
+commit2 = Comment.create!(body: "Splitwise clone", user_id: dom.id, post_id: post2.id)
+
+
+Like.create!(like_id: post1.id, like_type: "Post", user_id: demo.id)
+Like.create!(like_id: post1.id, like_type: "Post", user_id: joe.id)
+Like.create!(like_id: post1.id, like_type: "Post", user_id: dom.id)
+Like.create!(like_id: commit1.id, like_type: "Comment", user_id: dom.id)
+Like.create!(like_id: commit2.id, like_type: "Comment", user_id: dom.id)
+
